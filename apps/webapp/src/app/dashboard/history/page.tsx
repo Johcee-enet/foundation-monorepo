f9ffc3916ed0fe4a4f8bc5a17af3af3ba4bf168d
Miracle Friday <miraclef60@gmail.com>
2024-06-12 19:45:49 +0100

    refactor: refactored history page

--- apps/webapp/src/app/dashboard/history/page.tsx
+++ apps/webapp/src/app/dashboard/history/page.tsx
@@ -40,136 +40,53 @@ export default History;
 const HistoryItem: FC<{ activities: Doc<"activity">[] | undefined }> = ({
   activities,
 }) => {
+  if (!activities) {
+
+    return (
+      <div>
+        <Loader color="white" />
+
+      </div>
+    )
+  }
+
+
+  if (activities.length === 0) {
+    return (
+      <span className="text-lg text-center my-auto mx-auto text-white">No recorded history yet</span>
+    )
+  }
+
+
   return (
     <ul className="space-y-2">
-      {activities &&
-        activities.map((history, i) => (
-          <li className="history-event">
-            <div className="flex items-start gap-3">
-              <div
-                className={`p-2 text-3xl ${
-                  history.type == "xp" && "bg-[#E2DEF0] text-[#5F37E6]"
-                } ${
-                  history.type == "rank" && "bg-[#D5EEF0] text-[#14BBCC]"
+      {!!activities.length && activities.map((history) => (
+        <li className="history-event">
+          <div className="flex items-start gap-3">
+            <div
+              className={`p-2 text-3xl ${history.type == "xp" && "bg-[#E2DEF0] text-[#5F37E6]"
+                } ${history.type == "rank" && "bg-[#D5EEF0] text-[#14BBCC]"
                 } w-fit rounded-md`}
-              >
-                {history.type == "xp" && <BiRedo />}
-                {history.type == "rank" && <IoTelescopeOutline />}
-              </div>
-              <div className="max-w-64">
-                <h3 className="text-lg font-medium">{history.message}</h3>
-                <span className="text-base text-[#989898]">
-                  {new Date(history._creationTime).toLocaleDateString("en-US", {
-                    //   hour: "2-digit",
-                    //   minute: "2-digit",
-                    dateStyle: "full",
-                  })}
-                </span>
-              </div>
+            >
+              {history.type == "xp" && <BiRedo />}
+              {history.type == "rank" && <IoTelescopeOutline />}
             </div>
-            {history.type == "xp" && (
-              <h3 className="text-lg font-semibold">{history.extra} XP</h3>
-            )}
-          </li>
-        ))}
+            <div className="max-w-64">
+              <h3 className="text-lg font-medium">{history.message}</h3>
+              <span className="text-base text-[#989898]">
+                {new Date(history._creationTime).toLocaleDateString("en-US", {
+                  //   hour: "2-digit",
+                  //   minute: "2-digit",
+                  dateStyle: "full",
+                })}
+              </span>
+            </div>
+          </div>
+          {history.type == "xp" && (
+            <h3 className="text-lg font-semibold">{history.extra} XP</h3>
+          )}
+        </li>
+      ))}
     </ul>
   );
 };
-const historyActivities = [
-  {
-    activity: "Johcee joined using your referral link",
-    value: "1234",
-    date: 1212,
-    type: "referral",
-  },
-  {
-    activity: "Johcee joined using your referral link",
-    value: "1234",
-    date: 1212,
-    type: "leaderboard",
-  },
-  {
-    activity: "Johcee joined using your referral link",
-    value: "1234",
-    date: 1212,
-    type: "leaderboard",
-  },
-  {
-    activity: "Johcee joined using your referral link",
-    value: "1234",
-    date: 1212,
-    type: "referral",
-  },
-  {
-    activity: "Johcee joined using your referral link",
-    value: "1234",
-    date: 1212,
-    type: "referral",
-  },
-  {
-    activity: "Johcee joined using your referral link",
-    value: "1234",
-    date: 1212,
-    type: "referral",
-  },
-  {
-    activity: "Johcee joined using your referral link",
-    value: "1234",
-    date: 1212,
-    type: "leaderboard",
-  },
-  {
-    activity: "Johcee joined using your referral link",
-    value: "1234",
-    date: 1212,
-    type: "leaderboard",
-  },
-  {
-    activity: "Johcee joined using your referral link",
-    value: "1234",
-    date: 1212,
-    type: "referral",
-  },
-  {
-    activity: "Johcee joined using your referral link",
-    value: "1234",
-    date: 1212,
-    type: "leaderboard",
-  },
-  {
-    activity: "Johcee joined using your referral link",
-    value: "1234",
-    date: 1212,
-    type: "leaderboard",
-  },
-  {
-    activity: "Johcee joined using your referral link",
-    value: "1234",
-    date: 1212,
-    type: "referral",
-  },
-  {
-    activity: "Johcee joined using your referral link",
-    value: "1234",
-    date: 1212,
-    type: "referral",
-  },
-  {
-    activity: "Johcee joined using your referral link",
-    value: "1234",
-    date: 1212,
-    type: "referral",
-  },
-  {
-    activity: "Johcee joined using your referral link",
-    value: "1234",
-    date: 1212,
-    type: "leaderboard",
-  },
-  {
-    activity: "Johcee joined using your referral link",
-    value: "1234",
-    date: 1212,
-    type: "leaderboard",
-  },
-];
