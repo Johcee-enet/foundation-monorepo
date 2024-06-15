2b8a52bfc4961ed818d5693bc227b6a8e253ce3f
Miracle Friday <miraclef60@gmail.com>
2024-06-15 18:36:17 +0100

    feat: added analytics and speed-insights packages from nextjs

--- apps/webapp/src/app/page.tsx
+++ apps/webapp/src/app/page.tsx
@@ -3,6 +3,8 @@
 import { useEffect } from "react";
 import Image from "next/image";
 import { useRouter, useSearchParams } from "next/navigation";
+import { SpeedInsights } from "@vercel/speed-insights/next"
+import { Analytics } from "@vercel/analytics/react";
 
 export default function Home() {
   const router = useRouter();
@@ -20,35 +22,39 @@ export default function Home() {
     }, 3000);
   }, []);
   return (
-    <main className="flex flex-col items-center pb-10">
-      <div className="my-auto flex shrink-0 flex-col items-center justify-center gap-3">
-        <Image
-          src="/foundation.svg"
-          alt="Logo"
-          height={130}
-          width={130}
-          priority
-          className="shrink-0 invert dark:invert-0"
-        />
-        <div className="relative h-14 w-64 shrink-0 object-contain">
+    <html lang="en">
+      <main className="flex flex-col items-center pb-10">
+        <div className="my-auto flex shrink-0 flex-col items-center justify-center gap-3">
           <Image
-            src="/foundation-text.png"
+            src="/foundation.svg"
+            alt="Logo"
+            height={130}
+            width={130}
+            priority
+            className="shrink-0 invert dark:invert-0"
+          />
+          <div className="relative h-14 w-64 shrink-0 object-contain">
+            <Image
+              src="/foundation-text.png"
+              alt="Logo"
+              fill={true}
+              sizes="100%"
+              className="invert-0 dark:invert"
+            />
+          </div>
+        </div>
+        <div className="relative h-16 w-40 shrink-0 justify-self-end object-contain">
+          <Image
+            src="/powered.png"
             alt="Logo"
             fill={true}
             sizes="100%"
-            className="invert-0 dark:invert"
+            className="invert dark:invert-0"
           />
         </div>
-      </div>
-      <div className="relative h-16 w-40 shrink-0 justify-self-end object-contain">
-        <Image
-          src="/powered.png"
-          alt="Logo"
-          fill={true}
-          sizes="100%"
-          className="invert dark:invert-0"
-        />
-      </div>
-    </main>
+      </main>
+      <SpeedInsights />
+      <Analytics />
+    </html>
   );
 }
